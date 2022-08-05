58803a0758c23ddf15a5fb06117f0cb399beb360
Somprasong Damyos <somprasong.damyos@gmail.com>
2022-08-05 11:43:46 +0700

    handle error start daemon process

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
 	"github.com/somprasongd/go-thai-smartcard/pkg/model"
 	"github.com/somprasongd/go-thai-smartcard/pkg/server"
 	"github.com/somprasongd/go-thai-smartcard/pkg/smc"
@@ -28,6 +34,23 @@ func main() {
 	smc := smc.NewSmartCard()
 	err := smc.StartDaemon(broadcast, opts)
 	if err != nil {
-		panic(err)
+		log.Println("Error occurred while start daemon process, press Ctrl+C to exit.")
+		go func() {
+			for {
+				message := model.Message{
+					Event: "smc-error",
+					Payload: map[string]string{
+						"message": fmt.Sprintf("Error occurred while start daemon process, %v", err.Error()),
+					},
+				}
+				broadcast <- message
+				time.Sleep(1 * time.Second)
+			}
+		}()
+		// Listen for syscall signals for process to interrupt/quit
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, os.Interrupt)
+		s := <-sig
+		log.Printf("Received %v signal...", s)
 	}
 }
